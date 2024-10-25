ffa713feff18bc45b1087e3b2218fd91dfdbf8fd
[Emmanard] <[emmanard9@gmail.com]>
2024-10-25 11:18:34 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App';
-// import Starrating from './Starrating';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
-    {/* <Starrating maxRating={5} defaultRating={3} message={["okay", "Better","Best", "Great","Perfect"]}/>
-    <Starrating size={24} />
-     */}
   </React.StrictMode>
 );
 
